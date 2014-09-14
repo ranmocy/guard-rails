ab8c9b6b58f41c48da6c92679db272e98a9d1949
Ranmocy Sheng <ranmocy@gmail.com>
2014-09-13 21:59:14 -0700

    specify test coverage method by ENV CI

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,10 @@
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
-# require 'simplecov'
-# SimpleCov.start
+if ENV['CI']
+  require "codeclimate-test-reporter"
+  CodeClimate::TestReporter.start
+else
+  require 'simplecov'
+  SimpleCov.start
+end
 
 require 'guard/rails'
 
